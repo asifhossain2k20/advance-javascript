4bca73ba160ec2af1cc43e47d2a0a561d3081690
asifhossain2k20 <59142263+asifhossain2k20@users.noreply.github.com>
2020-08-06 23:18:42 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,30 @@
+const value=0;
+if(value){
+    console.log("It is True");
+}
+else{
+    console.log("It is false");
+}
+
+const value2="";
+if(value2){
+    console.log("It is True");
+}
+else{
+    console.log("It is false");
+}
+
+const value3=[];
+if(value3){
+    console.log("It is True");
+}
+else{
+    console.log("It is false");
+}
+let value4={};
+if(value4){
+    console.log("It is True");
+}
+else{
+    console.log("It is false");
+}
